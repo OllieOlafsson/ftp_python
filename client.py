56c021993585229a80ddcb9207fbeeca55d0d95e
OllieOlafsson <118857030+OllieOlafsson@users.noreply.github.com>
2025-05-04 14:40:19 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -3,16 +3,62 @@ import socket
 import sys
 import os
 
-IP = '172.20.10.6'
-PORT = 8999
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    with open("/home/franco/Documents/Coding/Python/File Transfer server/a4229412966_10.jpg", 'rb') as file:
-        file_size = os.path.getsize("/home/franco/Documents/Coding/Python/File Transfer server/a4229412966_10.jpg")
-        print(file_size)
-        data = file.read(file_size)
+IP = '192.168.1.137'
+PORT = 8000
+MSGLEN = 4096
+file_location = './flag.gif'
+count = 0
 
-        s.connect((IP, PORT))
-        s.sendall(data)
-        #print(data)
-        print(sys.getsizeof(data))
+def receive(s):
+    chunks = []
+    bytes_received = 0
+    while bytes_received < MSGLEN:
+        chunk = s.recv(MSGLEN)
+        if chunk == b'':
+            break
+        chunks.append(chunk)
+        bytes_received = bytes_received + len(chunk)
+        return b''.join(chunks)
+
+def transmit(s, msg):
+    totalsent = 0
+    while totalsent < MSGLEN:
+        sent = s.send(msg[totalsent:]) 
+        if sent == 0:
+            break
+        totalsent = totalsent + sent
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((IP, PORT))
+    s.settimeout(5)
+
+    operation = input("Options: \n1. Upload a file to server\n2. Download a file from server\n>>").encode('utf-8')
+    SYN = transmit(s, operation)      #send option to socket
+    # Upload
+    if (operation == b'1'):     
+        with open(file_location, 'rb') as file:
+            ACK = receive(s)        #Receive acknowledgement of selection
+            print(ACK)
+
+            file_size = os.path.getsize(file_location)
+            print(file_size)
+            data = file.read(file_size)
+         
+            PAYLOAD = transmit(s, data)       #send data
+            SYN_ACK = receive(s)    # ack wether data was uploaded successfully
+            print(type(SYN_ACK))
+
+
+    # Download
+    elif (operation == b'2'):
+        ACK = receive(s)      # Receive acknowledgement message
+        print(ACK)
+
+        count += 1
+        TREE_OUTPUT = receive(s)
+        #print(f"{count}. {reply.decode()}")
+        selection = input("Select file (by number): ").encode('utf-8')
+        transmit(selection)
+
 
--- server.py
+++ server.py
@@ -1,21 +1,70 @@
-#server
 import socket
+import threading
+import os
 
 HOST = ''
-PORT = 8999
-print("- Server has started. -")
+PORT = 8000
+MSGLEN = 4096
+path = '.'
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        print(conn)
-        with conn:
-            with open('output', 'wb') as file:   
-                while True:
-                    data = conn.recv(4096)
-                    file.write(data)
-                    print(f"Received data {data}.")
-                    if not data: break
+def client_download(conn, addr):
+    tree = bytearray()
+    for item in os.listdir(path):
+        tree = item.encode('utf-8') 
+        print(transmit(conn, tree))
+        print(tree)
+    file_selection = receive(conn)
+    index = int(file_selection.decode())
+    print(index)
 
+def client_upload(conn, addr):
+    #Writing to 'output' file client is sending
+    count = 0
+    with open("output", 'wb') as file:
+        while True:
+            data = receive(conn)
+            count += 1
+            if not data:break
+            file.write(data) 
+            print (f"[{count}] Received {MSGLEN}")
+
+def transmit(conn, msg):
+    totalsent = 0
+    while totalsent < MSGLEN:
+        sent = conn.send(msg[totalsent:]) 
+        if sent == 0:
+            break
+        totalsent = totalsent + sent
+
+def receive(conn):
+    chunks = []
+    bytes_received = 0
+    while bytes_received < MSGLEN:
+        chunk = conn.recv(MSGLEN)
+        if chunk == b'':
+            break
+        chunks.append(chunk)
+        bytes_received = bytes_received + len(chunk)
+        return b''.join(chunks)
+        
+def start():
+    print("- Server has started -")
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server.bind((HOST, PORT))
+            server.listen()
+            conn, addr = server.accept()
+            with conn:
+                # Operation selection from client
+                operation = receive(conn)
+                print(f"Client has selected option {operation.decode('utf-8')}")
+                if (operation == b'1'):
+                    transmit(conn, b'Upload operation selected')
+                    client_upload(conn, addr)
+                    transmit(conn, b'Data was uploaded successfully')
+                elif (operation == b'2'):
+                    transmit(conn, b'Download operation selected')
+                    client_download(conn, addr)
+
+start()
